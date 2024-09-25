d7aae40e73b5086e2ba206fc834c7db318a0fc6b
AkulGumudavall <gumudaau@mail.uc.edu>
2024-09-24 20:31:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your party affiliation, please input R/D/I");
+        String partyAffiliation = input.nextLine();
+
+            if(partyAffiliation.equals("D")){
+                System.out.println("You get the Democratic Donkey");
+            }else if(partyAffiliation.equals("R")){
+                System.out.println("You get the Republican Elephant");
+            }else if(partyAffiliation.equals("I")){
+                System.out.println("You get the Independent Man");
+            }else{
+                System.out.println("You get other");
+            }
+
+    }
+
+}
