233689caae480f9d8fa9fd998cb4391cb7957b06
AkulGumudavall <gumudaau@mail.uc.edu>
2024-09-29 15:37:48 -0400

    Fixed stuff again

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("What is your party affiliation, please input R/D/I");
+        //String partyAffiliation
         String partyAffiliation;
+        //Output "What is your party affiliation, please input R/D/I"
+        System.out.println("What is your party affiliation, please input R/D/I");
         if(input.hasNextLine()){
+            //input partyAffiliation
             partyAffiliation = input.nextLine();
+            //if partyAffiliation == "D" then
+                //Output "You get the Democratic Donkey"
             if(partyAffiliation.equals("D")){
                 System.out.println("You get the Democratic Donkey");
-            }else if(partyAffiliation.equals("R")){
+
+            }
+            //else if partyAffiliation == "R" then
+                //Output "You get the Republican Elephant"
+            else if(partyAffiliation.equals("R")){
                 System.out.println("You get the Republican Elephant");
-            }else if(partyAffiliation.equals("I")){
+            }
+            //else if partyAffiliation == "I" then
+                //Output "You get the Independent Man"
+            else if(partyAffiliation.equals("I")){
                 System.out.println("You get the Independent Man");
-            }else{
+            }
+            //else
+                //Output "You get other"
+            else{
                 System.out.println("You get other");
             }
 
